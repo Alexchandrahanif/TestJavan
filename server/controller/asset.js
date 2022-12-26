5101cecf2428a1d699b4ded0bd634387f2a66319
Alexchandrahanif <alexchandra1998@gmail.com>
2022-12-26 15:00:35 +0700

    get all asset and get asset by id done

--- server/controller/asset.js
+++ server/controller/asset.js
@@ -1,2 +1,39 @@
-class Controller {}
+const { User, Asset } = require("../models/index");
+
+class Controller {
+  // GET ALL ASSETS
+  static async getAssets(req, res, next) {
+    try {
+      const dataAsset = await Asset.findAll({
+        include: User,
+      });
+      res.status(200).json({
+        statusCode: 200,
+        data: dataAsset,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // GET ASSET BY ID
+  static async getAsset(req, res, next) {
+    try {
+      const { id } = req.params;
+      const dataAsset = await Asset.findOne({
+        where: {
+          id,
+        },
+        include: User,
+      });
+
+      res.status(200).json({
+        statusCode: 200,
+        data: dataAsset,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
 module.exports = Controller;
--- server/routes/asset.js
+++ server/routes/asset.js
@@ -1,3 +1,7 @@
+const Controller = require("../controller/asset");
+
 const assetRouter = require("express").Router();
 
+assetRouter.get("/", Controller.getAssets);
+assetRouter.get("/:id", Controller.getAsset);
 module.exports = assetRouter;

1b0230f3a4c357207557d871b2f2637ef3eb099b
Alexchandrahanif <alexchandra1998@gmail.com>
2022-12-26 13:54:29 +0700

    get user by id

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,9 +1,29 @@
 const { User, Asset } = require("../models/index");
 class Controller {
+  // GET ALL USERS
   static async getUSers(req, res, next) {
     console.log("okey");
     try {
       const dataUser = await User.findAll({
+        include: Asset,
+      });
+      res.status(200).json({
+        statusCode: 200,
+        data: dataUser,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // GET USER BY ID
+  static async getUser(req, res, next) {
+    try {
+      const { id } = req.params;
+      const dataUser = await User.findOne({
+        where: {
+          id,
+        },
         include: Asset,
       });
       res.status(200).json({
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,6 @@ const Controller = require("../controller/user");
 const userRouter = require("express").Router();
 
 userRouter.get("/", Controller.getUSers);
+userRouter.get("/:id", Controller.getUser);
 
 module.exports = userRouter;
